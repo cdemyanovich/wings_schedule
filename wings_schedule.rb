412dd803ec1591a1861ac61d642eb270175dac20
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2008-10-12 20:14:53 -0400

    Added TV broadcasts to event description

--- wings_schedule.rb
+++ wings_schedule.rb
@@ -10,12 +10,18 @@ games = schedule.search('tr.oddRowColor | tr.evenRowColor')
 games.each do |game|
   raw_details = game.children_of_type('td')
   details = raw_details.map { |d| d.inner_text.to_s.strip }
-  # details.each_with_index { |d, i| puts "[#{i}] = #{d}" }
+  
+  broadcasts = []
+  broadcasts << "National: #{details[7]}" unless details[7].empty?
+  broadcasts << "Local(H): #{details[9]}" unless details[9].empty?
+  broadcasts << "Local(A): #{details[8]}" unless details[8].empty?
+  
   event = Icalendar::Event.new
   event.summary = "#{details[1]} @ #{details[2]}"
   event.dtstart = DateTime.parse("#{details[0]} at #{details[5]}")
   event.duration = "PT3H" # 3 hours
-    
+  event.description = broadcasts.join("; ")
+  
   calendar.add_event(event)
 end
 
